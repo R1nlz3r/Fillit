13ba30770045dee195b850a58c4fe0edd30d473f
Mathis Pandelé <mapandel@student.42.fr>
2017-01-10 19:33:07 +0100

    main.c 1.1 : Add init t_fillit

--- sources/main.c
+++ sources/main.c
@@ -6,18 +6,30 @@
 /*   By: vlanduyt <vlanduyt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 17:25:51 by vlanduyt          #+#    #+#             */
-/*   Updated: 2017/01/10 17:36:50 by vlanduyt         ###   ########.fr       */
+/*   Updated: 2017/01/10 19:27:05 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+static t_fillit		*ft_init_fillit(t_fillit *fillit)
+{
+	if (!(fillit = ft_memalloc(sizeof(t_fillit)))
+		|| (fillit->fd = open(argv[2], 2)) == -1)
+		exit(-1);
+	return (fillit);
+}
+
 int		main(int argc, char **argv)
 {
+	t_fillit	*fillit;
+
+	fillit = NULL;
 	if (argc != 2)
 	{
 		ft_putendl("usage: fillit target_file");
-		return (-1);
+		exit(-1);
 	}
+	fillit = ft_init_fillit(fillit);
 	return (0);
 }
