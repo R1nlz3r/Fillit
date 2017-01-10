55b2aa374e769e3cb9bec524beda920197d49064
Mathis Pandelé <mapandel@student.42.fr>
2017-01-10 19:32:14 +0100

    fillit.h 1.1 : Add t_fillit

--- sources/fillit.h
+++ sources/fillit.h
@@ -6,7 +6,7 @@
 /*   By: vlanduyt <vlanduyt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 17:17:22 by vlanduyt          #+#    #+#             */
-/*   Updated: 2017/01/10 17:24:53 by vlanduyt         ###   ########.fr       */
+/*   Updated: 2017/01/10 19:18:18 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 # define FILLIT_H
 
 # include "../libft/libft.h"
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+typedef struct		s_fillit
+{
+	int				fd;
+}					t_fillit;
 
 #endif
