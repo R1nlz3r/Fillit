0138d3cfb4529f2e661e3034960f6bfc4f3ed6fe
Mathis Pandelé <mapandel@student.42.fr>
2017-01-21 19:19:32 +0100

    main.c 1.3 : Init fillit->tetri

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: vlanduyt <vlanduyt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 17:25:51 by vlanduyt          #+#    #+#             */
-/*   Updated: 2017/01/15 01:32:00 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/01/21 19:15:33 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 static t_fillit		*ft_init_fillit(t_fillit *fillit, char **argv)
 {
 	if (!(fillit = ft_memalloc(sizeof(t_fillit)))
-		|| (fillit->fd = open(argv[1], 2)) == -1)
+		|| (fillit->fd = open(argv[1], 2)) == -1
+		|| !(fillit->tetri = ft_memalloc(sizeof(t_fillit_tetri))))
 		exit(-1);
-	fillit->tetri = NULL;
 	fillit->tetristart = NULL;
 	fillit->countlines = 0;
 	fillit->counttetri = 0;
