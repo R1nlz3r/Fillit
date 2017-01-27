76d9c9eddba91da54bdd6230e1d93c56dd8e1ff1
Mathis Pandelé <mapandel@student.42.fr>
2017-01-27 06:21:20 +0100

    split_tetriminos.c 1.4 : Reprise + Bug fix + Check caracs

--- sources/split_tetriminos.c
+++ sources/split_tetriminos.c
@@ -6,40 +6,67 @@
 /*   By: vlanduyt <vlanduyt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 19:41:48 by vlanduyt          #+#    #+#             */
-/*   Updated: 2017/01/21 19:18:09 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/01/27 06:18:16 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-void	ft_split_tetriminos(t_fillit *fillit)
+static void		ft_check_carac_tetriminos(t_fillit *fillit)
 {
-	while ((get_next_line(fillit->fd, &fillit->line)))
+	fillit->counttetri = 0;
+	while (fillit->counttetri < 26)
 	{
-		if (fillit->countlines < 5 && ft_strlen(fillit->line) == 4
-			&& fillit->counttetri < 26)
+		fillit->countlines = 0;
+		fillit->countsharps = 0;
+		while (fillit->countlines < 4)
 		{
-			if (fillit->countlines == 0
-				&& !(fillit->tetri->piece = ft_memalloc(sizeof(char**) * 4)))
-				exit (-1);
-			fillit->tetri->piece[fillit->countlines] = ft_strdup(fillit->line);
+			fillit->countcaracs = 0;
+			while (fillit->countcaracs < 4 && fillit->tetri[fillit->counttetri]
+				[fillit->countlines][fillit->countcaracs])
+			{
+				if (fillit->tetri[fillit->counttetri][fillit->countlines]
+					[fillit->countcaracs] == '#')
+					++fillit->countsharps;
+				if (!(fillit->tetri[fillit->counttetri][fillit->countlines]
+					[fillit->countcaracs] == '#'
+					|| fillit->tetri[fillit->counttetri][fillit->countlines]
+					[fillit->countcaracs] == '.') || fillit->countsharps > 4)
+						exit (-1);
+				++fillit->countcaracs;
+			}
 			++fillit->countlines;
 		}
-		else if (fillit->countlines == 4 && !fillit->line[0])
+		++fillit->counttetri;
+	}
+	//Maintenant go check si les dieses sont bien collés(serrés) entre eux
+}
+
+void	ft_split_tetriminos(t_fillit *fillit)
+{
+	while (1)
+	{
+		if (fillit->countlines < 4 && fillit->counttetri < 26
+			&& (fillit->lenline = read(fillit->fd, fillit->line, 5))
+			&& ft_strlen(fillit->line) == 5 && fillit->line[4] == '\n')
+		{
+			ft_strncpy(fillit->tetri[fillit->counttetri]
+				[fillit->countlines++], fillit->line, 4);
+			ft_strclr(fillit->line);
+		}
+		else if (fillit->countlines == 4
+			&& (fillit->lenline = read(fillit->fd, fillit->line, 1))
+			&& ft_strlen(fillit->line) == 1 && fillit->line[0] == '\n')
 		{
 			fillit->countlines = 0;
-			if (!fillit->tetristart)
-				fillit->tetristart = fillit->tetri;
-			if (!(fillit->tetri->next = ft_memalloc(sizeof(t_fillit_tetri*))))
-				exit (-1);
-			fillit->tetri = fillit->tetri->next;
 			++fillit->counttetri;
 		}
+		else if (!fillit->lenline)
+			break ;
 		else
 			exit (-1);
 	}
-	//go check si les tériminos sont bien des tétriminos
-	//+ si tous les caractères sont bien formatés
-	//(ça pourrait être utile)
-	//check si \n final en plus --> erreur
+	if (fillit->countlines != 4)
+		exit (-1);
+	ft_check_carac_tetriminos(fillit);
 }
