03ea3019a5da49ae43d1768055a103934f69efaf
Mathis Pandelé <mapandel@student.42.fr>
2017-01-28 03:51:23 +0100

    split_tetriminos.c 1.5 : Check tetriminos valide + Nouveaux noms de variables

--- sources/split_tetriminos.c
+++ sources/split_tetriminos.c
@@ -6,67 +6,87 @@
 /*   By: vlanduyt <vlanduyt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 19:41:48 by vlanduyt          #+#    #+#             */
-/*   Updated: 2017/01/27 09:43:37 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/01/28 03:35:23 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static void		ft_check_carac_tetriminos(t_fillit *fillit)
+static void		ft_check_valid_tetriminos(t_fillit *f)
 {
-	fillit->counttetri = 0;
-	while (fillit->counttetri < 26 && fillit->tetri[fillit->counttetri]
-		[fillit->countlines][fillit->countcaracs])
+	f->tet = 0;
+	while (f->tet < 26 && f->tetri[f->tet][f->line][f->carac])
 	{
-		fillit->countlines = 0;
-		fillit->countsharps = 0;
-		while (fillit->countlines < 4)
+		f->line = 0;
+		while (f->line < 4)
 		{
-			fillit->countcaracs = 0;
-			while (fillit->countcaracs < 4)
+			f->carac = 0;
+			while (f->carac < 4)
 			{
-				if (fillit->tetri[fillit->counttetri][fillit->countlines]
-					[fillit->countcaracs] == '#')
-					++fillit->countsharps;
-				else if (!(fillit->tetri[fillit->counttetri][fillit->countlines]
-					[fillit->countcaracs] == '.') || fillit->countsharps > 4)
+				if (f->tetri[f->tet][f->line][f->carac] == '#'
+					&& !((f->carac != 0 && (f->tetri[f->tet][f->line][f->carac - 1] == '#'))
+					|| (f->line != 0 && (f->tetri[f->tet][f->line - 1][f->carac] == '#'))
+					|| (f->line != 4 && (f->tetri[f->tet][f->line + 1][f->carac] == '#'))
+					|| (f->carac != 4 && (f->tetri[f->tet][f->line][f->carac + 1] == '#'))))
 						exit (-1);
-				++fillit->countcaracs;
+				++f->carac;
 			}
-			++fillit->countlines;
+			++f->line;
 		}
-		if (fillit->countsharps < 4)
-			exit (-1);
-		++fillit->counttetri;
+		++f->tet;
 	}
-	//Maintenant go check si les dieses sont bien collés(serrés) entre eux
 }
 
-void	ft_split_tetriminos(t_fillit *fillit)
+static void		ft_check_carac_tetriminos(t_fillit *f)
+{
+	f->tet = 0;
+	while (f->tet < 26 && f->tetri[f->tet][f->line][f->carac])
+	{
+		f->line = 0;
+		f->sharp = 0;
+		while (f->line < 4)
+		{
+			f->carac = 0;
+			while (f->carac < 4)
+			{
+				if (f->tetri[f->tet][f->line][f->carac] == '#')
+					++f->sharp;
+				else if (!(f->tetri[f->tet][f->line][f->carac] == '.')
+					|| f->sharp > 4)
+					exit (-1);
+				++f->carac;
+			}
+			++f->line;
+		}
+		if (f->sharp < 4)
+			exit (-1);
+		++f->tet;
+	}
+	ft_check_valid_tetriminos(f);
+}
+
+void	ft_split_tetriminos(t_fillit *f)
 {
 	while (1)
 	{
-		if (fillit->countlines < 4 && fillit->counttetri < 26
-			&& (fillit->lenline = read(fillit->fd, fillit->line, 5))
-			&& ft_strlen(fillit->line) == 5 && fillit->line[4] == '\n')
+		if (f->line < 4 && f->tet < 26 && (f->lenline = read(f->fd, f->buf, 5))
+			&& ft_strlen(f->buf) == 5 && f->buf[4] == '\n')
 		{
-			ft_strncpy(fillit->tetri[fillit->counttetri]
-				[fillit->countlines++], fillit->line, 4);
-			ft_strclr(fillit->line);
+			ft_strncpy(f->tetri[f->tet][f->line++], f->buf, 4);
+			ft_strclr(f->buf);
 		}
-		else if (fillit->countlines == 4
-			&& (fillit->lenline = read(fillit->fd, fillit->line, 1))
-			&& ft_strlen(fillit->line) == 1 && fillit->line[0] == '\n')
+		else if (f->line == 4 && (f->lenline = read(f->fd, f->buf, 1))
+			&& ft_strlen(f->buf) == 1 && f->buf[0] == '\n')
 		{
-			fillit->countlines = 0;
-			++fillit->counttetri;
+			f->line = 0;
+			++f->tet;
 		}
-		else if (!fillit->lenline)
+		else if (!f->lenline)
 			break ;
 		else
 			exit (-1);
 	}
-	if (fillit->countlines != 4)
+	if (f->line != 4)
 		exit (-1);
-	ft_check_carac_tetriminos(fillit);
+	ft_check_carac_tetriminos(f);
 }
