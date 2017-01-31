be5054419ca7922640a0c32fd39fedbdacd6fa6c
Mathis Pandelé <mapandel@student.42.fr>
2017-01-31 12:09:42 +0100

    find_solution.c 1.1 : 2re partie - Sans backtrack

--- sources/find_solution.c
+++ sources/find_solution.c
@@ -6,18 +6,71 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/28 05:28:22 by mapandel          #+#    #+#             */
-/*   Updated: 2017/01/29 10:20:19 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/01/31 12:02:31 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+#include <stdio.h>
 
-static int		ft_try_soluce(t_fillit *f)
+static void		ft_put_on_map(t_fillit *f)
 {
-	//à toi de jouer (c'est l'heure du dududu-duel)
-	//return (1) pour indiquer qu'on a réussi à tout rentrer
-	//sinon return(0) pour incrémenter la taille de la map
-	(void)f;//<- retire ça, c'est juste pour utiliser le paramètre et passer le flag -Werror
+	f->line = f->postetrix[f->tet];
+	while (f->line < 4)
+	{
+		f->carac = f->postetriy[f->tet];
+		while (f->carac < 4)
+		{
+			if (f->tetri[f->tet][f->line][f->carac] == '#')
+				f->map[f->line + f->mapx - f->postetrix[f->tet]][f->carac + f->mapy - f->postetriy[f->tet]] = 'A' + f->tet;
+			++f->carac;
+		}
+		++f->line;
+	}
+}
+
+static int		ft_try_to_put_on_map(t_fillit *f)
+{
+	f->line = f->postetrix[f->tet];
+	while (f->line < 4)
+	{
+		f->carac = f->postetriy[f->tet];
+		while (f->carac < 4)
+		{
+			if (f->tetri[f->tet][f->line][f->carac] == '#'
+				&& (f->line - f->postetrix[f->tet] + f->mapx >= f->mapsize
+				|| f->carac - f->postetriy[f->tet] + f->mapy >= f->mapsize
+				|| f->map[f->line - f->postetrix[f->tet] + f->mapx][f->carac - f->postetriy[f->tet] + f->mapy] != '.'))
+				return (0);
+			++f->carac;
+		}
+		++f->line;
+	}
+	ft_put_on_map(f);
+	return (1);
+}
+
+static int		ft_iter_on_map(t_fillit *f)
+{
+	f->tet = 0;
+	while (f->tet < 26 && f->tetri[f->tet][0][0])
+	{
+		f->mapx = 0;
+		f->booltetonmap = 0;
+		while (f->mapx < f->mapsize && !f->booltetonmap)
+		{
+			f->mapy = 0;
+			while (f->mapy < f->mapsize && !f->booltetonmap)
+			{
+				f->booltetonmap = ft_try_to_put_on_map(f);
+				++f->mapy;
+			}
+			++f->mapx;
+		}
+		if (!f->booltetonmap)
+			return (0);
+		++f->tet;
+	}
 	return (1);
 }
 
@@ -35,16 +88,12 @@ static int		ft_init_map(t_fillit *f)
 		f->map[i] = ft_strfill(f->map[i], '.', f->mapsize);
 		++i;
 	}
-	return (ft_try_soluce(f));
+	return (ft_iter_on_map(f));
 }
 
 void	ft_find_solution(t_fillit *f)
 {
 	while(!(ft_init_map(f)))
 		++f->mapsize;
+	ft_putmap(f->map, f->mapsize);
 }
-
-//find_solution						check (on incrémente la map ici)
-//init_map							check
-	//try_soluce (aka poser tous les tétriminos sur la map et voir si ça rentre)
-		//crush_on_solution (Pour poser un tétriminos sur la map)
