ba3170a038a17383775800ff7e763b42dde7b83f
Mathis PANDELE <mapandel@e1r2p20.42.fr>
2017-02-03 14:06:03 +0100

    fillit.h 1.10 : Norme

--- sources/fillit.h
+++ sources/fillit.h
@@ -6,7 +6,7 @@
 /*   By: vlanduyt <vlanduyt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 17:17:22 by vlanduyt          #+#    #+#             */
-/*   Updated: 2017/02/03 11:39:59 by vlanduyt         ###   ########.fr       */
+/*   Updated: 2017/02/03 13:07:48 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ typedef struct	s_fillit
 	int		booltetonmap;
 }				t_fillit;
 
-void			ft_find_solution(t_fillit *f);
+int				ft_init_map(t_fillit *f);
 void			ft_exit(void);
 void			ft_split_tetriminos(t_fillit *f);
 
